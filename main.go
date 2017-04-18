b1bf360b2742de6b5a46fe056b7c01785e0da697
aerth <aerth@riseup.net>
2017-04-18 21:47:12 +0000

    unix socket listen

--- main.go
+++ main.go
@@ -1,24 +1,29 @@
 package main
 
 import (
+	"os"
+
 	diamond "github.com/aerth/diamond/lib"
 	"github.com/aertx/tlde/srv"
-	"os"
 )
 
 var config = diamond.ConfigFields{
-	Addr:     "0.0.0.0:" + os.Getenv("PORT"),
-	Name:     "tl;de",
-	Level:    3,
-	Socket:   "/tmp/tlde.socket",
-	Kicks:    true,
-	Kickable: true,
+	Addr:       "0.0.0.0:" + os.Getenv("PORT"),
+	SocketHTTP: os.Getenv("SOCKET"),
+	Name:       "tl;de",
+	Level:      3,
+	Socket:     "/tmp/tlde.socket",
+	Kicks:      true,
+	Kickable:   true,
 }
 
 func main() {
 	handler := srv.NewMux()
 	server := diamond.NewServer()
 	server.Config = config
+	if os.Getenv("PORT") == "" {
+		server.Config.Addr = ""
+	}
 	server.SetMux(handler)
 	err := server.Start()
 	if err != nil {
